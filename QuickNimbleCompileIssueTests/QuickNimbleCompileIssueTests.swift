25b3a92fe24d4b2ebda54643f82343ee64ff0e87
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-09-28 00:49:12 +0700

    Initial Commit

---
+++ QuickNimbleCompileIssueTests/QuickNimbleCompileIssueTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuickNimbleCompileIssueTests.swift
+//  QuickNimbleCompileIssueTests
+//
+//  Created by Ricardo Pramana Suranta on 9/28/17.
+//  Copyright © 2017 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import XCTest
+@testable import QuickNimbleCompileIssue
+
+class QuickNimbleCompileIssueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
