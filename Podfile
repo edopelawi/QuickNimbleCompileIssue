52687652ba59fea03f9bea9d3424e2a61285ebb3
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2017-09-28 01:00:22 +0700

    Add Quick and Nimble pods

---
+++ Podfile
@@ -0,0 +1,8 @@
+use_frameworks!
+
+target 'QuickNimbleCompileIssueTests' do
+  inherit! :search_paths
+
+  pod 'Quick', '1.1.0'
+  pod 'Nimble', '7.0.2'
+end
---
+++ Pods/Target Support Files/Pods-QuickNimbleCompileIssueTests/Pods-QuickNimbleCompileIssueTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuickNimbleCompileIssueTests : NSObject
+@end
+@implementation PodsDummy_Pods_QuickNimbleCompileIssueTests
+@end
